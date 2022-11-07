22c25491376e3b11612a4aff8bca39f048723d7c
saumyanayak <saumyanayak264@gmail.com>
2022-11-07 16:28:40 +0530

    UI & Functionality working fine

--- components/SendContract.jsx
+++ components/SendContract.jsx
@@ -8,7 +8,7 @@ import styles from "../styles/Send.module.css"
 import Lottie from "react-lottie"
 import uploadedAnimationData from "../assets/uploaded-animation.json"
 import uploadAnimationData from "../assets/upload-animation.json"
-import { Button, BeatLoader, ButtonGroup, Input } from "@chakra-ui/react"
+import { Link, Button, BeatLoader, ButtonGroup, Input, Box, useClipboard } from "@chakra-ui/react"
 import { useDropzone } from "react-dropzone"
 
 export default function SendContract() {
@@ -24,23 +24,6 @@ export default function SendContract() {
         setFileImg(acceptedFiles[0])
     }, [])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
-    // draggableFileArea.addEventListener("drop", (e) => {
-    //     console.log("File Dropped Successfully!")
-    //     setFileImg(
-    //         e.target.files[0].name + " " + (e.target.files[0].size / 1024).toFixed(1) + " KB"
-    //     )
-    //     console.log(
-    //         e.target.files[0].name + " " + (e.target.files[0].size / 1024).toFixed(1) + " KB"
-    //     )
-    //     // let files = e.dataTransfer.files;
-    //     // fileInput.files = files;
-    //     console.log(document.querySelector(".default-file-input").value)
-    //     // fileName.innerHTML = files[0].name;
-    //     // fileSize.innerHTML = (files[0].size/1024).toFixed(1) + " KB";
-    //     // uploadedFile.style.cssText = "display: flex;";
-    //     // progressBar.style.width = 0;
-    //     // fileFlag = 0;
-    // })
 
     const [ipfsFileHash, setIpfsFileHash] = useState()
     const [generatedCode, setGeneratedCode] = useState()
@@ -58,6 +41,8 @@ export default function SendContract() {
 
     const [isSendActive, setIsSendActive] = useState(true)
     const [animate, setAnimate] = useState(false)
+
+    const { onCopy, value, setValue, hasCopied } = useClipboard("")
 
     const uploadAnimationdefaultOptions = {
         loop: true,
@@ -78,58 +63,29 @@ export default function SendContract() {
     }
 
     const retrieve = async function () {
-        setIsLoading(true)
-        const address = sendContractAddress
-        console.log(code)
-        const ethers = Moralis.web3Library
-        const web3Provider = await Moralis.enableWeb3()
-        const signer = web3Provider.getSigner()
-        const contract = new ethers.Contract(address, abi, signer)
-
-        const transaction = await contract.getFile(code?.toString(), {
-            gasLimit: 2500000,
-        })
         let transactionReceipt
         try {
+            setIsLoading(true)
+            const address = sendContractAddress
+            console.log(code)
+            const ethers = Moralis.web3Library
+            const web3Provider = await Moralis.enableWeb3()
+            const signer = web3Provider.getSigner()
+            const contract = new ethers.Contract(address, abi, signer)
+
+            const transaction = await contract.getFile(code?.toString(), {
+                gasLimit: 2500000,
+            })
             transactionReceipt = await transaction.wait(1)
+            setRetrievedFileHash(transactionReceipt.events[0].args.fileHash)
+            console.log(transactionReceipt.events[0].args.fileHash)
+            setIsLoading(false)
         } catch (err) {
             console.log("F : ", err)
+            handleNewNotification("File Downloaded Once Already!")
+            setIsLoading(false)
         }
-        setRetrievedFileHash(transactionReceipt.events[0].args.fileHash)
-        setIsLoading(false)
     }
-    // const {
-    //     runContractFunction: uploadedFile,
-    //     isLoading: isUploading,
-    //     isFetching: isFetchingUploading,
-    // } = useWeb3Contract({
-    //     abi: abi,
-    //     contractAddress: sendContractAddress,
-    //     functionName: "uploadedFile",
-    //     gasLimit: 2500000,
-    //     // msgValue: entranceFee,
-    //     params: { ipfsFileHash: ipfsFileHash?.toString() },
-    // })
-
-    // const {
-    //     runContractFunction: getFile,
-    //     isLoading: isGettingFile,
-    //     isFetching: isFetchingGettingFile,
-    // } = useWeb3Contract({
-    //     abi: abi,
-    //     contractAddress: sendContractAddress,
-    //     functionName: "getFile",
-    //     // msgValue: entranceFee,
-    //     params: { randNum: code?.toString() },
-    // })
-
-    /* View Functions */
-    // const { runContractFunction: getRandomNum } = useWeb3Contract({
-    //     abi: abi,
-    //     contractAddress: sendContractAddress,
-    //     functionName: "getRandomNum",
-    //     params: {},
-    // })
 
     const handleNewNotification = (message) => {
         dispatch({
@@ -141,9 +97,16 @@ export default function SendContract() {
         })
     }
 
+    const openFile = (e) => {
+        e.preventDefault()
+        console.log(`https://ipfs.io/ipfs/${ipfsFileHash}`)
+        window.open(`https://ipfs.io/ipfs/${ipfsFileHash}`, "_blank")
+    }
+
     const sendFileToIPFS = async (e) => {
         e.preventDefault()
         setIsUploading(true)
+        console.log("Uploading file...")
         console.log(fileImg)
         if (fileImg) {
             try {
@@ -173,6 +136,7 @@ export default function SendContract() {
         // console.log(ipfsFileHash)
         const send = async function () {
             try {
+                console.log("Getting Code...")
                 setIsGettingCode(true)
                 const address = sendContractAddress
                 console.log(address)
@@ -185,6 +149,7 @@ export default function SendContract() {
                     console.log("RequestFulfilled")
                     let code = await contract.getRandomNum()
                     setGeneratedCode(code)
+                    setValue(code)
                     handleNewNotification("Generated Code")
                     setIsGettingCode(false)
                 })
@@ -199,15 +164,12 @@ export default function SendContract() {
                 setIsGettingCode(false)
             }
         }
+        console.log("Uploaded File")
         ipfsFileHash && send()
     }, [ipfsFileHash])
 
     const [action, setAction] = useState("send")
 
-    const changeActionType = (e) => {
-        e.preventDefault()
-        console.log("styles.card")
-    }
     return (
         <>
             {sendContractAddress ? (
@@ -223,111 +185,116 @@ export default function SendContract() {
                                 <p>This image is stored on IPFS & The Ethereum Blockchain!</p>
                                 <img src={`https://ipfs.io/ipfs/${ipfsFileHash}`} alt="" />
                             </>
-                            
                         )} */}
                         <div className={styles.sendContainer}>
-                            <form encType="multipart/form-data">
-                                {/* <div className={styles.uploadFilesContainer}> */}
-                                {fileImg ? (
-                                    <div className={styles.dragFileArea}>
-                                        <Lottie
-                                            options={uploadedAnimationdefaultOptions}
-                                            height={200}
-                                            width={200}
-                                        />
-                                        <div className={styles.fileBlock}>
-                                            <p className={styles.fileName}>
-                                                {`${fileImg.name} | ${(
-                                                    fileImg.size / 1024
-                                                ).toFixed(1)}KB`}
-                                            </p>{" "}
-                                        </div>
+                            {fileImg ? (
+                                <div className={styles.dragFileArea}>
+                                    <Lottie
+                                        options={uploadedAnimationdefaultOptions}
+                                        height={200}
+                                        width={200}
+                                    />
+                                    <div className={styles.fileBlock}>
+                                        <p className={styles.fileName}>
+                                            {`${fileImg.name} | ${(fileImg.size / 1024).toFixed(
+                                                1
+                                            )}KB`}
+                                        </p>{" "}
                                     </div>
-                                ) : (
-                                    <div className={styles.dragFileArea} {...getRootProps()}>
-                                        <input type="file" {...getInputProps()} />{" "}
-                                        <Lottie
-                                            options={uploadAnimationdefaultOptions}
-                                            height={200}
-                                            width={200}
-                                        />
-                                        <p style={{ fontSize: "1rem" }}>
-                                            {" "}
-                                            Drag & drop any file here or{" "}
-                                            <span className={styles.browseFilesText}>
-                                                browse
-                                            </span>{" "}
-                                            file from device
-                                        </p>
-                                        {/* <label className={styles.label}>
-                                                <span className={styles.browseFiles}>
-
-                                                </span>{" "}
-                                            </label> */}
-                                    </div>
-                                )}
-                                <div className={styles.sendButtonContainer}>
-                                    {fileImg ? (
-                                        <Button
-                                            onClick={sendFileToIPFS}
-                                            type="submit"
-                                            colorScheme="purple"
-                                            borderRadius="40px"
-                                            w="50%"
-                                            isLoading={isGettingCode}
-                                        >
-                                            {generatedCode ? generatedCode : "Send"}
-                                        </Button>
-                                    ) : (
-                                        <Button
-                                            onClick={sendFileToIPFS}
-                                            colorScheme="purple"
-                                            borderRadius="40px"
-                                            w="50%"
-                                            isLoading={isUploading}
-                                            isDisabled={fileImg ? false : true}
-                                        >
-                                            Upload
-                                        </Button>
-                                    )}
                                 </div>
-                            </form>
-                        </div>
-                        <div className={styles.receiveContainer}>
-                            <div>
-                                <Input
-                                    // placeholder="Code"
-                                    size="md"
-                                    onChange={(e) => setCode(e.target.value)}
-                                />
-                                {retrievedFileHash ? (
-                                    <a href={`https://ipfs.io/ipfs/${ipfsFileHash}`} download>
-                                        File
-                                    </a>
+                            ) : (
+                                <div className={styles.dragFileArea} {...getRootProps()}>
+                                    <input type="file" {...getInputProps()} />{" "}
+                                    <Lottie
+                                        options={uploadAnimationdefaultOptions}
+                                        height={200}
+                                        width={200}
+                                    />
+                                    <p style={{ fontSize: "1rem" }}>
+                                        {" "}
+                                        Drag & drop any file here or{" "}
+                                        <span className={styles.browseFilesText}>browse</span> file
+                                        from device
+                                    </p>
+                                </div>
+                            )}
+                            <div className={styles.sendButtonContainer}>
+                                {generatedCode ? (
+                                    <Box
+                                        borderRadius="40px"
+                                        w="100%"
+                                        display="flex"
+                                        flexDirection="row"
+                                        alignItems="center"
+                                        justifyContent="space-between"
+                                    >
+                                        <Input
+                                            w={"70%"}
+                                            isReadOnly={true}
+                                            value={generatedCode}
+                                            // mu={"10px"}
+                                            // mr={2}
+                                        />
+                                        <Button onClick={onCopy} w={"25%"} fontSize="0.8rem">
+                                            {hasCopied ? "Copied!" : "Copy"}
+                                        </Button>
+                                    </Box>
                                 ) : (
                                     <Button
+                                        onClick={sendFileToIPFS}
                                         colorScheme="purple"
                                         borderRadius="40px"
                                         w="50%"
-                                        mt="16px"
-                                        onClick={retrieve}
-                                        disabled={isLoading}
+                                        isDisabled={!fileImg}
+                                        isLoading={isGettingCode || isUploading}
                                     >
-                                        Get File
+                                        send
                                     </Button>
                                 )}
                             </div>
-                            {/* <div>{`The file hash is : ${fileHash}`}</div> */}
+                        </div>
+                        <div className={styles.receiveContainer}>
+                            <Input
+                                // placeholder="Code"
+                                size="md"
+                                onChange={(e) => setCode(e.target.value)}
+                            />
+                            {/* {retrievedFileHash ? (
+                                <Link
+                                    href={`https://ipfs.io/ipfs/${ipfsFileHash}`}
+                                    // rel="noreferrer"
+                                    // target="_blank"
+                                    style={{
+                                        height: "40px",
+                                        width: "50%",
+                                    }}
+                                >
+                                    <Box
+                                        bgColor="#6B46C1"
+                                        color="white"
+                                        borderRadius="40px"
+                                        h="100%"
+                                        w="100%"
+                                    >
+                                        open file
+                                    </Box>
+                                </Link>
+                            ) : ( */}
+                            <Button
+                                colorScheme="purple"
+                                borderRadius="40px"
+                                w="50%"
+                                mt="16px"
+                                onClick={retrievedFileHash ? openFile : retrieve}
+                                isLoading={isLoading}
+                            >
+                                {retrievedFileHash ? "open file" : "get file"}
+                            </Button>
+                            {/* )} */}
                         </div>
                     </div>
                 </div>
             ) : (
-                // <div className={styles.card}>
-                //     <div className={styles.content}>
-                //         <div className={styles.front}>Front</div>
-                //         <div className={styles.back}>Back!</div>
-                //     </div>
-                // </div>
                 <div>Please connect to a supported chain </div>
             )}
             <div
@@ -349,7 +316,7 @@ export default function SendContract() {
                     isDisabled={isSendActive}
                     onClick={() => setIsSendActive(true)}
                 >
-                    Send
+                    send
                 </Button>
                 <Button
                     onClick={() => setIsSendActive(false)}
@@ -358,7 +325,7 @@ export default function SendContract() {
                     variant="unstyled"
                     isDisabled={!isSendActive}
                 >
-                    Receive
+                    receive
                 </Button>
             </div>
         </>
