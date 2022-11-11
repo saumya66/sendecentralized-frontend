98d1928f9dd75723f53ea93a59cf0bf496cfc5f6
saumyanayak <saumyanayak264@gmail.com>
2022-11-12 02:33:17 +0530

    Added env var

--- components/SendContract.jsx
+++ components/SendContract.jsx
@@ -116,8 +116,8 @@ export default function SendContract() {
                     url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
                     data: formData,
                     headers: {
-                        pinata_api_key: `${process.env.PINATA_API_KEY}`,
-                        pinata_secret_api_key: `${process.env.PINATA_API_SECRET}`,
+                        pinata_api_key: `${process.env.NEXT_PUBLIC_PINATA_API_KEY}`,
+                        pinata_secret_api_key: `${process.env.NEXT_PUBLIC_PINATA_API_SECRET}`,
                         "Content-Type": "multipart/form-data",
                     },
                 })
