8c68ed0aa92264aaf6a979b86a63316b80a5cd40
saumyanayak <saumyanayak264@gmail.com>
2022-11-11 21:26:12 +0530

    please work

--- next.config.js
+++ next.config.js
@@ -4,5 +4,32 @@ const nextConfig = {
 
 module.exports = {
     nextConfig,
-    future: { webpack5: true },
+
+    accordion: ["../src/**/*.accordion.@(j|t)sx"],
+    addons: ["@chakra-ui/accordion"],
+    framework: "@accordion/react",
+    features: {
+        emotionAlias: false,
+    },
+    webpackFinal: async (config) => {
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: "javascript/auto",
+        })
+
+        return config
+    },
+    rules: [
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                },
+            },
+        },
+    ],
 }
